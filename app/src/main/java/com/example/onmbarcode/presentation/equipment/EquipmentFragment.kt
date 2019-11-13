87878bab47867d3034df11081841d0fbea2ab423
ZianeA <wit_wit90@hotmail.com>
2019-11-13 16:02:50 +0100

    Add circular reveal animation for equipment color

--- app/src/main/java/com/example/onmbarcode/presentation/equipment/EquipmentFragment.kt
+++ app/src/main/java/com/example/onmbarcode/presentation/equipment/EquipmentFragment.kt
@@ -4,11 +4,14 @@ package com.example.onmbarcode.presentation.equipment
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewAnimationUtils
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
@@ -23,6 +26,7 @@ import com.google.android.material.animation.ArgbEvaluatorCompat
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_equipment.view.*
 import javax.inject.Inject
+import kotlin.math.hypot
 
 /**
  * A simple [Fragment] subclass.
@@ -52,20 +56,39 @@ class EquipmentFragment : Fragment(), EquipmentView {
         recyclerView.setItemSpacingDp(EQUIPMENT_ITEM_SPACING)
         // TODO refactor, move to EpoxyModel
         epoxyController = EquipmentEpoxyController(View.OnClickListener {
+            val cardView = it as CardView
             val greenColor = ContextCompat.getColor(it.context, R.color.materialGreen)
             val redColor = ContextCompat.getColor(it.context, R.color.materialRed)
-            val cardView = it as CardView
 
             if (cardView.cardBackgroundColor.defaultColor == greenColor)
                 return@OnClickListener
 
-            ObjectAnimator.ofObject(
-                cardView,
-                "cardBackgroundColor",
-                ArgbEvaluator(),
-                redColor,
-                greenColor
-            ).start()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                //TODO refactor everything
+                //get the center for the clipping circle relative to view.
+                val revealView = cardView.findViewById<ImageView>(R.id.revealView)
+                val cx = revealView.width / 2
+                val cy = revealView.height / 2
+
+                // get the final radius for the clipping circle
+                val finalRadius = hypot(cx.toDouble(), cy.toDouble()).toFloat()
+
+                // create the animator for this view (the start radius is zero)
+                val anim =
+                    ViewAnimationUtils.createCircularReveal(revealView, 0, cy, 0f, finalRadius * 2)
+
+                // start the animation
+                revealView.visibility = View.VISIBLE
+                anim.start()
+            } else {
+                ObjectAnimator.ofObject(
+                    cardView,
+                    "cardBackgroundColor",
+                    ArgbEvaluator(),
+                    redColor,
+                    greenColor
+                ).start()
+            }
         })
 
         return rootView
